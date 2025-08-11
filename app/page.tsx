3fc6401e8f94bb92102d74ef6c8b18ba3f26aeec
harsh07may <harsh07may@gmail.com>
2025-08-12 02:45:02 +0530

    Updated Resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { AnimatedShinyText } from "./components/AnimatedShinyText";
+import { cn } from "./lib/utils";
 
 export default function Page() {
   return (
@@ -39,6 +40,28 @@ export default function Page() {
           </a>
           .
         </p>
+      </div>
+      <div className="z-10 flex min-h-64 items-center gap-2.5">
+        <div
+          className={cn(
+            "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+          )}
+        >
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+            <a href="/Resume.pdf" download>
+              Resume ✨
+            </a>
+          </AnimatedShinyText>
+        </div>
+        {/* <div
+          className={cn(
+            "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+          )}
+        >
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+            <span>Contact 📞</span>
+          </AnimatedShinyText>
+        </div> */}
       </div>
     </section>
   );
